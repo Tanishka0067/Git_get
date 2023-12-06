f3e556caa6ecfef80b199075cb4b7c0fb0b15c45
Tanishka Agarwal <144484154+Tanishka0067@users.noreply.github.com>
2023-12-06 23:50:34 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,10 @@
-import logo1 from './assets/githublogo.png';
 export default function Navbar(){
     return(
        <>
         <div className="nav">
            <div className="element1">
            <div className="logo">
-            <img src={logo1} alt="github" />
+            <img src="src\assets\githublogo.png" alt="github" />
            </div>
             <div className="logoname">GitGet</div>
            </div>
